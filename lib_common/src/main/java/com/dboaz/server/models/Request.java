ad635ae52e5b4b8b0dac5e5fe6f97db106a3454c
GustavoBoaz <gustavo.boaz@hotmail.com>
2024-06-10 10:54:06 -0300

    20240610/Gustavo.Boaz/Feature-REFACTORY request class

--- lib_common/src/main/java/com/dboaz/server/models/Request.java
+++ lib_common/src/main/java/com/dboaz/server/models/Request.java
@@ -14,7 +14,6 @@ import org.apache.logging.log4j.LogManager;
 import com.dboaz.utils.enums.SeverityEnum;
 import com.dboaz.utils.errors.GenericException;
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 public class Request {
@@ -34,10 +33,16 @@ public class Request {
     this.headers = new HashMap<>();
     this.body = new HashMap<>();
 
-    parseRequest(convertInputStreamInString(in));
+    fillRequest(parseString(in));
   }
 
-  protected String convertInputStreamInString(InputStream in) {
+  /**
+   * parseString
+   * 
+   * @param in - input stream format protocol http request
+   * @return string representation of the request
+   */
+  protected String parseString(InputStream in) {
     byte[] buffer = new byte[BUFFER_SIZE];
     int bytesRead;
     StringBuilder request = new StringBuilder();
@@ -54,59 +59,111 @@ public class Request {
     return request.toString();
   }
 
-  protected void parseRequest(String inputStream) {
+  /**
+   * fillRequest
+   * 
+   * @param inputStream - String input stream
+   */
+  protected void fillRequest(String inputStream) {
     try (Scanner scanner = new Scanner(inputStream)) {
-      // Lê a primeira linha (method, path, protocol)
-      String firstLine = scanner.nextLine();
-      String[] firstLineParts = firstLine.split("\\s+");
-      method = firstLineParts[0];
-      String pathWithParams = firstLineParts[1];
-      protocol = firstLineParts[2];
-
-      // Extrai os parâmetros de consulta
-      int questionMarkIndex = pathWithParams.indexOf('?');
-      if (questionMarkIndex != -1) {
-        path = pathWithParams.substring(0, questionMarkIndex);
-        String paramssString = pathWithParams.substring(questionMarkIndex + 1);
-        String[] paramssArray = paramssString.split("&");
-        for (String paramsPair : paramssArray) {
-          String[] pair = paramsPair.split("=");
-          params.put(pair[0], pair[1]);
-        }
-      } else {
-        path = pathWithParams;
-      }
-
-      String line;
-      while (!(line = scanner.nextLine()).isEmpty()) {
-          int colonIndex = line.indexOf(": ");
-          if (colonIndex != -1) {
-              String headerName = line.substring(0, colonIndex);
-              String headerValue = line.substring(colonIndex + 2); // 2 é o comprimento de ": "
-              headers.put(headerName, headerValue);
-          } else {
-              // Lidar com cabeçalhos malformados ou inválidos, se necessário
-          }
-      }
-
-      // Lê o body
-      StringBuilder bodyBuilder = new StringBuilder();
-      while (scanner.hasNextLine()) {
-        bodyBuilder.append(scanner.nextLine());
-      }
-      String bodyString = bodyBuilder.toString();
-
-      //valida se tem conteudo no body
-      if (bodyString.length() > 0) {
-        // Converte o body para um Map
-        Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, Object>>() {
-        }.getType();
-        body = gson.fromJson(bodyString, type);
-      }
-
-    } catch (JsonSyntaxException e) {
-      LOGGER.error(new GenericException("[ Json format body ]: ".concat(e.getMessage()), SeverityEnum.SEV_004, 400));
+      fillFirstLine(scanner);
+      fillHeaderLines(scanner);
+      fillBody(scanner);
+    } catch (Exception e) {
+      LOGGER.error(new GenericException("[ Fill request invalid ]: ".concat(e.getMessage()), SeverityEnum.SEV_004, 500));
     }
+  }
+
+  /**
+   * fillFirstLine
+   * 
+   * @param scanner - First reading
+   */
+  protected void fillFirstLine(Scanner scanner) {
+    String scannerFirstLine = scanner.nextLine();
+    String[] firstLineParts = scannerFirstLine.split("\\s+");
+    fillMethod(firstLineParts[0]);
+    fillPathAndParams(firstLineParts[1]);
+    fillProtocol(firstLineParts[2]);
+  }
+
+  /**
+   * fillHeaderLines
+   * 
+   * @param scanner - Second reading
+   */
+  protected void fillHeaderLines(Scanner scanner) {
+    String line;
+    while (!(line = scanner.nextLine()).isEmpty()) {
+      int colonIndex = line.indexOf(": ");
+      if (colonIndex != -1) {
+        String headerName = line.substring(0, colonIndex);
+        String headerValue = line.substring(colonIndex + 2);
+        headers.put(headerName, headerValue);
+      } else {
+        LOGGER.error(new GenericException("[ Poorly formatted header ]: valid format is 'key': 'value' format", SeverityEnum.SEV_004, 400));
+      }
+    }
+  }
+
+  /**
+   * fillBody
+   * 
+   * @param scanner - Third reading
+   */
+  protected void fillBody(Scanner scanner) {
+    StringBuilder bodyBuilder = new StringBuilder();
+    String bodyString = bodyBuilder.toString();
+
+    // Read body
+    while (scanner.hasNextLine()) {
+      bodyBuilder.append(scanner.nextLine());
+    }
+
+    // Check if body exists
+    if (bodyString.length() > 0) {
+      // Convert body to map
+      Gson gson = new Gson();
+      Type type = new TypeToken<Map<String, Object>>() {}.getType();
+      body = gson.fromJson(bodyString, type);
+    }
+  }
+
+  /**
+   * fillMethod
+   * 
+   * @param firstLineParts0 - String ex.: GET
+   */
+  protected void fillMethod(String firstLinePart0) {
+    this.method = firstLinePart0;
+  }
+
+  /**
+   * fillPathAndParams
+   * 
+   * @param firstLineParts1 - String ex.: /api/v1/auction?page=1&size=100
+   */
+  protected void fillPathAndParams(String firstLinePart1) {
+    int questionMarkIndex = firstLinePart1.indexOf('?');
+    if (questionMarkIndex != -1) {
+      path = firstLinePart1.substring(0, questionMarkIndex);
+      String paramssString = firstLinePart1.substring(questionMarkIndex + 1);
+      String[] paramssArray = paramssString.split("&");
+      for (String paramsPair : paramssArray) {
+        String[] pair = paramsPair.split("=");
+        params.put(pair[0], pair[1]);
+      }
+    } else {
+      path = firstLinePart1;
+    }
+  }
+
+  /**
+   * fillProtocol
+   * 
+   * @param firstLineParts2 - String ex.: HTTP/1.1
+   */
+  protected void fillProtocol(String firstLinePart2) {
+    protocol = firstLinePart2;
   }
 }
