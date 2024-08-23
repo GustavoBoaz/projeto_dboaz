84c9f9c7d30556da65ca7bbd0a54029cb926ce9f
GustavoBoaz <gustavo.souza@ab-inbev.com>
2024-08-23 13:29:38 -0300

    20240823/Gustavo.Boaz/Feature-ADD cases spring

--- qa_acceptance/src/test/java/com/dboaz/SpringAcceptanceTest.java
+++ qa_acceptance/src/test/java/com/dboaz/SpringAcceptanceTest.java
@@ -5,7 +5,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
-@SpringBootTest
+@SpringBootTest(classes = {
+    MainAppGateway.class,
+    MainAppBid.class,
+    MainAppAuth.class,
+    MainAppAuction.class,
+    MainAppPayment.class,
+    MainAppProduct.class,
+    MainAppProfile.class,
+    MainAppLogistic.class,
+    MainAppMessaging.class,
+    MainAppNotification.class,
+    MainAppCommentRating.class
+})
 public class SpringAcceptanceTest {
 
 }
