9ecfc5d0140d36726782cf48469e8eaf34c27e53
GustavoBoaz <gustavo.souza@ab-inbev.com>
2024-09-02 14:59:10 -0300

    Gustavo.Boaz/UPDATE-add cucumber reports for github action

--- qa_acceptance/src/test/java/com/dboaz/CucumberAcceptanceTest.java
+++ qa_acceptance/src/test/java/com/dboaz/CucumberAcceptanceTest.java
@@ -6,7 +6,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(
+    features = "src/test/resources/features",
+    plugin = "json:target/cucumber/report.json"
+)
 class CucumberAcceptanceTest {
 
 }
