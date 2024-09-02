0efe7975f939b494a76d50559e8befa4a6b8afce
Boaz Gustavo <gustavo.boaz@hotmail.com>
2024-09-02 16:26:34 -0300

    Gustavo.Boaz/UPDATE-CucumberAcceptanceTest.java

--- qa_acceptance/src/test/java/com/dboaz/CucumberAcceptanceTest.java
+++ qa_acceptance/src/test/java/com/dboaz/CucumberAcceptanceTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
-    plugin = "json:cucumber-report.json"
+    plugin = "json:target/cucumber-report.json"
 )
 class CucumberAcceptanceTest {
 
