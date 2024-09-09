ca2ba78704b7cf4c57c2b43241b2b80abfa13fac
GustavoBoaz <gustavo.souza@ab-inbev.com>
2024-09-09 19:06:51 -0300

    Gustavo.Boaz/Spike/Global-Security-APIs-CREATE contex RSA in base_core

--- base_core/src/main/java/com/dboaz/utils/security/RSAKeyGenerator.java
+++ base_core/src/main/java/com/dboaz/utils/security/RSAKeyGenerator.java
@@ -2,12 +2,14 @@ package com.dboaz.utils.security;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,13 +27,15 @@ public class RSAKeyGenerator {
     private static final String PATH = "base_core/src/main/resources";
     
     public static void main(String[] args) {
-        var pair = generateKeyPairRSA();
-
-        createFolderResourcesIfNotExist();
-        generateFileWithPublicKey(pair.getPublic());
-        generateFileWithPrivateKey(pair.getPrivate());
-
-        LOG.debug("Generate key RSA in " + PATH);
+        if (!keysExist()) {
+            var pair = generateKeyPairRSA();
+            createFolderResourcesIfNotExist();
+            generateFileWithPublicKey(pair.getPublic());
+            generateFileWithPrivateKey(pair.getPrivate());
+            LOG.debug("Generated RSA keys in " + PATH);
+        } else {
+            LOG.info("Keys already exist, no need to generate new ones.");
+        }
     }
 
     private static KeyPair generateKeyPairRSA() {
@@ -52,9 +56,13 @@ public class RSAKeyGenerator {
     }
 
     private static void generateFileWithPublicKey(PublicKey key) {
-        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(key.getEncoded());
-        try (FileOutputStream fos = new FileOutputStream(PATH + FILE_PUBLIC)) {
-            fos.write(x509EncodedKeySpec.getEncoded());
+        try {
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(key.getEncoded());
+            String publicKeyPEM = convertToPEMFormat("PUBLIC KEY", Base64.getEncoder().encodeToString(x509EncodedKeySpec.getEncoded()));
+
+            try (FileOutputStream fos = new FileOutputStream(PATH + FILE_PUBLIC)) {
+                fos.write(publicKeyPEM.getBytes(StandardCharsets.UTF_8));
+            }
         } catch (Exception e) {
             LOG.error(e.getMessage());
             var alert = new CustomAlert(SystemCodeEnum.C020DB);
@@ -63,13 +71,32 @@ public class RSAKeyGenerator {
     }
 
     private static void generateFileWithPrivateKey(PrivateKey key) {
-        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(key.getEncoded());
-        try (FileOutputStream fos = new FileOutputStream(PATH + FILE_PRIVATE)) {
-            fos.write(pkcs8EncodedKeySpec.getEncoded());
+        try {
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(key.getEncoded());
+            String privateKeyPEM = convertToPEMFormat("PRIVATE KEY", Base64.getEncoder().encodeToString(pkcs8EncodedKeySpec.getEncoded()));
+
+            try (FileOutputStream fos = new FileOutputStream(PATH + FILE_PRIVATE)) {
+                fos.write(privateKeyPEM.getBytes(StandardCharsets.UTF_8));
+            }
         } catch (Exception e) {
             LOG.error(e.getMessage());
             var alert = new CustomAlert(SystemCodeEnum.C020DB);
             throw GlobalException.builder().alert(alert).status(500).build();
         }
+    }
+
+    private static String convertToPEMFormat(String keyType, String base64EncodedKey) {
+        StringBuilder pemKey = new StringBuilder();
+        pemKey.append("-----BEGIN ").append(keyType).append("-----\n");
+        pemKey.append(base64EncodedKey.replaceAll("(.{64})", "$1\n")); // Divide a cada 64 caracteres
+        pemKey.append("\n-----END ").append(keyType).append("-----\n");
+        return pemKey.toString();
+    }
+
+    // Verifica se as chaves já existem
+    private static boolean keysExist() {
+        File publicKeyFile = new File(PATH + FILE_PUBLIC);
+        File privateKeyFile = new File(PATH + FILE_PRIVATE);
+        return publicKeyFile.exists() && privateKeyFile.exists();
     }
 }
