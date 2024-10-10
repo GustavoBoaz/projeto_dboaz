ae42261bf7f9082b412f06000756e8463c8c7dcf
GustavoBoaz <gustavo.souza@ab-inbev.com>
2024-10-09 23:19:28 -0300

    Gustavo.Boaz/Spike/MS-Auth-CREATE context auth

--- ms_auth/src/main/java/com/dboaz/resources/InfoAuthController.java
+++ ms_auth/src/main/java/com/dboaz/resources/InfoAuthController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Info")
@@ -29,14 +27,12 @@ public class InfoAuthController {
     @Value("${microservice.ms_auth.description}") String description;
 
     @Operation(
-    security = @SecurityRequirement(name = "bearer-key"),
     description = "Provide custom server info", responses = {
         @ApiResponse(responseCode = "200", description = "retrieve global info api", content = {
             @Content(mediaType = "application/json", schema = @Schema(implementation = GlobalInfo.class))
         }),
     })
     @GetMapping(path = Route.GET_INFO)
-    @PreAuthorize("hasAnyAuthority('SCOPE_DBO_ADM_DEV','SCOPE_DBO_ADM_QA')")
     public ResponseEntity<GlobalInfo> getInfo() {
         var result = GlobalInfo.builder().name(name).version(version).description(description).build();
 
